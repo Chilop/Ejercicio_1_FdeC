5eb4179631bdd44339ba8483e7030a87de4dd17d
Chilop <94477621+Chilop@users.noreply.github.com>
2022-05-06 10:40:26 -0500

    codigo del ejercicio
    faltan cosas al codigo

---
+++ Ejercicio_1_06_05_2022.cpp
@@ -0,0 +1,42 @@
+// Ejercicio_1_06_05_2022.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+/*
+Ejercicio 1
+Dada una cadena de m caracteres (m<n)
+llamda patron, encontrar una subcadena del texto que coincida con el patron 
+Dicho de otra manera, se requiere encontrar el indice i del caracter más a la izquierda de la primera subcadena que ccoincida con el texto.
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n, m;
+    string texto; // cadena de n caracteres
+    string patron; // cadena de m caracteres
+	string aux;
+	
+	cout << " dime cuantas oraciones quieres poner en la cadena texto:";
+		cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << " Escribe tu oracion" << endl;
+		texto[i];
+		getline(cin, texto);
+	}
+
+	for (int j = 0; j < n; j++)
+	{
+		cout << texto[j] << "" << endl;
+	}
+
+	if (patron < texto)
+	{
+
+	}
+
+	
+
+}
+
